a58519ddd0c415945b42b5ff4f206d2d621c07ae
Ирина Чебесова <54910331+chebesovairina@users.noreply.github.com>
2023-10-05 18:54:06 +0300

    Add files via upload

--- lab1/code/Results.java
+++ lab1/code/Results.java
@@ -5,18 +5,26 @@ import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-public class Results {
+public class Results 
+{
 
 	static void show_browse(String adding)
 	{
 		 
-        try{
-            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+        try
+        { //проверка на то, поддерживается ли класс и возможно ли открытие браузера
+            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) 
+            {
+            	//возвращает экземпляр контекста браузера с указанным URI
                 Desktop.getDesktop().browse(new URI("https://ru.wikipedia.org/w/index.php?curid="+URLEncoder.encode(adding, StandardCharsets.UTF_8)));
             }
-        } catch (IOException e) {
+        } 
+        catch (IOException e) 
+        {
             throw new RuntimeException(e);
-        } catch (URISyntaxException e) {
+        } 
+        catch (URISyntaxException e) 
+        {
             throw new RuntimeException(e);
         }
 	}
